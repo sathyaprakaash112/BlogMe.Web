a40eff7c4927488d3e4c264fe6cadce7143e6fcd
Sathya S <Sathya.S@odessainc.com>
2024-04-15 23:09:01 +0530

    Add project files.

---
+++ BlogMe/Data/BlogMeDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BlogMe.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogMe.Data
+{
+    public class BlogMeDbContext:DbContext
+    {
+        public BlogMeDbContext(DbContextOptions<BlogMeDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<BlogPostLike> BlogPostLike { get; set; }
+        public DbSet<BlogPostComment> BlogPostComment { get; set; }
+    }
+}
